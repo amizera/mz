56a6d078491452724d811b48a4aa15485a1e0049
Adam Mizera <amizera@gmail.com>
2022-11-21 10:03:13 +0100

    init2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import {Link} from 'react-router-dom'
+,
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Location from './Components/Location'
